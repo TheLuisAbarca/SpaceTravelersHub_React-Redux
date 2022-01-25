cf9104cc939a15a53b7290d812ad133ac9ac83ac
kerolous samy <keroloussamy98@gmail.com>
2022-01-25 17:30:38 +0200

    Call the API

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import Missions from './components/Missions/Missions';
 import Rockets from './components/Rockets/Rockets';
 import Profile from './components/Profile/Profile';
+import { getRocketsAction } from './redux/rockets/rockets';
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getRocketsAction());
+  }, []);
+
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/profile" element={<Profile />} />
-        </Route>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
