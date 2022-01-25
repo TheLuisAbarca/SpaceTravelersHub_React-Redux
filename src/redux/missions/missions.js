e2c89b78bf200b586c224ed31771a668371ea36d
TheLuisAbarca <arclaav.adm@gmail.com>
2022-01-25 11:37:07 -0500

    Implement fetching data mission from API.

--- src/redux/missions/missions.js
+++ src/redux/missions/missions.js
@@ -1,8 +1,40 @@
-const reducer = (state = {}, action) => {
+const FETCH_DATA = 'FETCH_DATA';
+
+const fetchMissions = (payload) => ({
+  type: FETCH_DATA,
+  payload,
+});
+
+export const getMissions = () => async (dispatch) => {
+  const response = await fetch('https://api.spacexdata.com/v3/missions', {
+    method: 'GET',
+  });
+  try {
+    const data = await response.json();
+    const newPayload = [];
+    data.forEach((mission) => {
+      newPayload.push({
+        mission_id: mission.mission_id,
+        mission_name: mission.mission_name,
+        mission_description: mission.description,
+      });
+    });
+    dispatch(fetchMissions(newPayload));
+    // dispatch({ type: FETCH_DATA, payload: newPayload });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const missionsReducer = (state = [], action) => {
   switch (action.type) {
+    case FETCH_DATA: {
+      console.log(action.payload);
+      return action.payload;
+    }
     default:
       return state;
   }
 };
 
-export default reducer;
+export default missionsReducer;
