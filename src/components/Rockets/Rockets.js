b246f55463d770ce433f46a1c35fc978043e97ca
kerolous samy <keroloussamy98@gmail.com>
2022-01-25 18:29:26 +0200

    Loop over rockets list

--- src/components/Rockets/Rockets.js
+++ src/components/Rockets/Rockets.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import RocketItem from '../RocketItem/RocketItem';
 
-const Rockets = () => (
-  <div>
-    <h1>Rockets</h1>
-  </div>
-);
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rocketsReducer);
+  return (
+    <div>
+      { rockets.map((rocket) => (<RocketItem key={rocket.id} rocket={rocket} />)) }
+    </div>
+  );
+};
 
 export default Rockets;
