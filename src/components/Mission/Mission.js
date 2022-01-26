7fb370c9eb4dad4820ed58129b7def3f6129c97d
TheLuisAbarca <arclaav.adm@gmail.com>
2022-01-26 10:48:47 -0500

    Add implementation of badges and add  more styling.

--- src/components/Mission/Mission.js
+++ src/components/Mission/Mission.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import Badge from 'react-bootstrap/Badge';
 import Button from 'react-bootstrap/Button';
 import { joinMission, leaveMission } from '../../redux/missions/missions';
+import style from './Mission.module.css';
 
 const Mission = ({ mission }) => {
   const dispatch = useDispatch();
@@ -15,14 +16,29 @@ const Mission = ({ mission }) => {
     }
   };
 
+  const badgeRender = () => {
+    if (mission.reserved) {
+      return <Badge bg="primary">Active Member</Badge>;
+    }
+    return <Badge bg="secondary" className={style.capitalized}>Not a member</Badge>;
+  };
+
+  const classMissionTitle = `align-top ${style.bold}`;
+
   return (
     <>
       <tr>
-        <td className="align-middle">{mission.mission_name}</td>
+        <td className={classMissionTitle}>{mission.mission_name}</td>
         <td>{mission.mission_description}</td>
-        <td className="align-middle"><Badge bg="primary">Active Member</Badge></td>
         <td className="align-middle">
-          <Button onClick={clickHandler} type="button" variant="outline-secondary">
+          {badgeRender()}
+        </td>
+        <td className="align-middle">
+          <Button
+            onClick={clickHandler}
+            type="button"
+            variant={(mission.reserved) ? 'outline-danger' : 'outline-secondary'}
+          >
             {(mission.reserved) ? 'Leave ' : 'Join '}
             Mission
           </Button>
