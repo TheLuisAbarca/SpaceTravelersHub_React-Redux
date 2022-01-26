5db19e2332532f738ab5183f0fcde413dc34b3d0
kerolous samy <keroloussamy98@gmail.com>
2022-01-26 18:16:48 +0200

    Add switch between badges and buttons

--- src/components/RocketItem/RocketItem.js
+++ src/components/RocketItem/RocketItem.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import Badge from 'react-bootstrap/Badge';
 import { useDispatch } from 'react-redux';
 import { bookRocket, cancelRocket } from '../../redux/rockets/rockets';
 import styles from './RocketItem.module.css';
 
 const RocketItem = ({ rocket }) => {
   const {
-    id, name, description, img,
+    id, name, description, img, reserved,
   } = rocket;
 
   const dispatch = useDispatch();
@@ -24,9 +25,12 @@ const RocketItem = ({ rocket }) => {
       <img src={img} alt={name} className={styles.img} />
       <div className={styles.cardBody}>
         <h3>{name}</h3>
-        <p>{description}</p>
-        <button type="button" onClick={bookRocketHandler} className={styles.btn}>Reserve Rocket</button>
-        <button type="button" onClick={cancelRocketHandler} className={styles.btn}>Cancel Reservation</button>
+        <p>
+          {reserved && (<Badge bg="info" className={styles.badge}>Reserved</Badge>)}
+          {description}
+        </p>
+        {!reserved && (<button type="button" onClick={bookRocketHandler} className={styles.btn}>Reserve Rocket</button>)}
+        {reserved && (<button type="button" onClick={cancelRocketHandler} className={styles['cancel-btn']}>Cancel Reservation</button>)}
       </div>
     </li>
   );
@@ -39,6 +43,7 @@ RocketItem.propTypes = {
     description: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
     img: PropTypes.string.isRequired,
+    reserved: PropTypes.bool,
   }).isRequired,
 };
 
